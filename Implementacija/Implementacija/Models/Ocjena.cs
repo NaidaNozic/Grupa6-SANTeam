0b482ec64d4cb2ad2bd1f01d2a46994bf8a9ec6d
NaidaNozic <92310390+NaidaNozic@users.noreply.github.com>
2022-06-14 18:09:33 +0200

    Odgovaranje na komentare radi

--- Implementacija/Implementacija/Models/Ocjena.cs
+++ Implementacija/Implementacija/Models/Ocjena.cs
@@ -6,8 +6,11 @@ namespace Implementacija.Models
     {
         [Key]
         public int Id { get; set; }
-        [Display(Name="Rank")]        
+        [Display(Name = "Rank")]
         public int OcjenaKorisnika { get; set; }
+
+        public string userId { get; set; }
+        public int movieId { get; set; }
         public Ocjena() { }
     }
 }
